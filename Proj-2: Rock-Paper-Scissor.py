935ecb413113c99fee2fd00a84f6dcfcd62a225b
Sayan Sarkar <72155617+ky13-troj@users.noreply.github.com>
2021-08-04 20:11:48 +0530

    Update Proj-2: Rock-Paper-Scissor.py

--- Proj-2: Rock-Paper-Scissor.py
+++ Proj-2: Rock-Paper-Scissor.py
@@ -1,5 +1,7 @@
+# Importing the library
 import random
 
+# Creating Figures
 rock = '''
     _______
 ---'   ____)
@@ -26,21 +28,20 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-game_images = [rock, paper, scissors]
+game_images = [rock, paper, scissors] # List of possible choices
 
-print("What do you choose? \n Type 0 for Rock \n Type 1 for Paper\n Type 2 for Scissors.\n ")
-while True:
-  user_choice = int(input("Enter your Choice : "))
+print("What do you choose? \n Type 0 for Rock \n Type 1 for Paper\n Type 2 for Scissors.\n ") # Printing Instruction about how to interact with the program
+while True: # Infinite loop is used otherwise after one run program will stop . 
+  user_choice = int(input("Enter your Choice : ")) # Taking user's choice
+ # Game Play Logic
   if user_choice >= 3 or user_choice < 0: 
-      print("You typed an invalid number, you lose!") 
+      print("You typed an invalid number, you lose!") # Avoiding error messages
   else:
       print(game_images[user_choice])
-
-      computer_choice = random.randint(0, 2)
+      computer_choice = random.randint(0, 2) # Choosing random integer in range of 0 to 2
       print("Computer chose:")
-      print(game_images[computer_choice])
-
-
+      print(game_images[computer_choice]) # Showing computer's chosen option
+      # Result
       if user_choice == 0 and computer_choice == 2:
           print("You win!")
       elif computer_choice == 0 and user_choice == 2:
@@ -51,6 +52,7 @@ while True:
           print("You win!")
       elif computer_choice == user_choice:
           print("It's a draw")
+  # Checking if the user wants to play the game again or not
   ask = input("want to continue ? Type Y for yes or N for no. ")
   if ask.lower() != 'y':
     break
