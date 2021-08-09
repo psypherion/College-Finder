a4a55a1a2c9d57bea766c00a34d86250c65486f7
Sayan Sarkar <72155617+ky13-troj@users.noreply.github.com>
2021-08-09 13:15:03 +0530

    Update Multiplication-Table.py

--- Multiplication-Table.py
+++ Multiplication-Table.py
@@ -1,5 +1,7 @@
 # Multiplication Table
 
+### Feature : Even if No term value is provided it will give 10 terms ###
+
 # Creating multiplication Function
 def mult(x, term = 10): # term by default is set to 10 i.e if the user does not specify number of terms, then by default 10 number swill be printed
     for i in range(1, term+1):
@@ -7,8 +9,19 @@ def mult(x, term = 10): # term by default is set to 10 i.e if the user does not 
         
 num = int(input("Enter the number : ")) # Taking the value of the number of which multiplication table will be generated
 term = input("Enter which number you want to create the table : ") # Number of terms wanted, Empty input value can also be provided
+print('''
 
+              _ _   _       _ _           _   _                  _____      _     _           
+  /\/\  _   _| | |_(_)_ __ | (_) ___ __ _| |_(_) ___  _ __      /__   \__ _| |__ | | ___    _ 
+ /    \| | | | | __| | '_ \| | |/ __/ _` | __| |/ _ \| '_ \ _____ / /\/ _` | '_ \| |/ _ \  (_)
+/ /\/\ | |_| | | |_| | |_) | | | (_| (_| | |_| | (_) | | | |_____/ / | (_| | |_) | |  __/   _ 
+\/    \/\__,_|_|\__|_| .__/|_|_|\___\__,_|\__|_|\___/|_| |_|     \/   \__,_|_.__/|_|\___|  (_)
+                     |_|                                                                      
+
+''')
+print('*'*20)
 if term == '': # Checking if the term input is Empty or not 
     mult(num)
 else: # If not empty then given number of terms will be generated
     mult(num, int(term))
+print('*'*20)
